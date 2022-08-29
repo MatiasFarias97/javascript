90385f22cc50184a2e620e488e5fc5ead4269357
Matias_Farias <mfmatias97@gmail.com>
2022-08-29 20:29:47 -0300

    Desafio opcional bucles

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const INICIO = prompt("Escribe la palabra INICIO para comenzar")
-let respuesta
- if(INICIO.toUpperCase == "INICIO"){
-    respuesta = prompt("En alto vive, en alto mora; teje que teje, la tejedora. La respuesta es...")
-        if(respuesta.toLocaleLowerCase == "la araña" || respuesta.toLocaleLowerCase == "araña"){
-            alert("Respuesta correcta")
-    }else{
-        alert("Respuesta incorrecta")
-    }
- }else{
-    alert("Vuelve cuando quieras jugar")
- }
+
+let respuesta = ""
+let salida = ""
+alert("Toca aceptar para ver la adivinanza del dia")
+while(respuesta != "araña"){
+    respuesta= prompt("En alto vive, en alto mora; teje que teje, la tejedora. La respuesta es...")
+}
+
+alert("Felicitaciones, has adivinado!.")

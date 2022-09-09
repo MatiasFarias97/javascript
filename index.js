c2b9a7563e4034bb05a54eca54a7ba04d479ea56
Matias_Farias <mfmatias97@gmail.com>
2022-09-08 21:15:12 -0300

    Entrega desafio opcional

--- index.js
+++ index.js
@@ -1,85 +1,26 @@
-function adivinanzaAraña(){
-    let respuesta = ""
-    let salida = ""
-    while(respuesta != "araña"){
-        respuesta = prompt("En alto vive, en alto mora; teje que teje, la tejedora. La respuesta es...").toLowerCase()
-        if(respuesta != "araña"){
-            salida = prompt("FALLASTE. Toca ACEPTAR para volver a intentar o ESC para salir").toLowerCase()
-        }else{
-            alert("Felicitaciones has adivinado!")
-            }
-        if(salida == "esc"){
-            break
-        }
+let arrayPersonas = []
+
+class Personas {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido
     }
 }
 
-function adivinanzaSilencio(){
-    let respuesta = ""
-    let salida = ""
-    while(respuesta != "el silencio"){
-        respuesta = prompt("Si lo nombro, lo rompo. La respuesta es...").toLowerCase()
-        if(respuesta != "el silencio"){
-            salida = prompt("FALLASTE. Toca ACEPTAR para volver a intentar o ESC para salir").toLowerCase()
-        }else{
-            alert("Felicitaciones has adivinado!")
-        }
-        if(salida == "esc"){
-            break
-        }
-    }
+function cargaDeDatos(){
+
+let inputNombre = prompt("Ingrese nombre")
+let inputApellido = prompt("Ingrese apellido")
+let inputPersona = new Personas(inputNombre, inputApellido) 
+arrayPersonas.push(inputPersona)
+
 }
 
-function adivinanzaMusica(){
-    let respuesta = ""
-    let salida = ""
-    while(respuesta != "la musica"){
-        respuesta = prompt("No la palpas pero si la oyes ¿Qué es?").toLowerCase()
-        if(respuesta != "la musica"){
-            salida = prompt("FALLASTE. Toca ACEPTAR para volver a intentar o ESC para salir").toLowerCase()
-        }else{
-            alert("Felicitaciones has adivinado!")
-            }
-        if(salida == "esc"){
-            break
-        }
-    }
+let salida = ""
+
+while(salida !== "salir"){
+    cargaDeDatos();
+    salida = prompt("Escriba salir si desea dejar de agregar").toLowerCase()   
 }
 
-function adivinanzaPeine(){
-    let respuesta = ""
-    let salida = ""
-    while(respuesta != "el peine"){
-        respuesta = prompt("Tiene dientes pero no muerde. ¿Qué es?").toLowerCase()
-        if(respuesta != "el peine"){
-            salida = prompt("FALLASTE. Toca ACEPTAR para volver a intentar o ESC para salir").toLowerCase()
-        }else{
-            alert("Felicitaciones has adivinado!")
-            }
-        if(salida == "esc"){
-            break
-        }
-    }
-}
-
-alert("Presiona ENTER para comenzar")
-
-let adivinanzas = parseInt(prompt("Hay cuatro adivinanzas, escriba el numero y aparecera. \n .1 \n .2 \n .3 \n .4"))
-
-switch(adivinanzas){
-    case 1:
-        adivinanzaAraña();
-    break
-    case 2:
-        adivinanzaMusica();
-    break
-    case 3:
-        adivinanzaPeine();
-    break
-    case 4:
-        adivinanzaSilencio();
-    break
-    default:
-        alert("Opcion desconocida")
-    break
-}
+console.log(arrayPersonas)

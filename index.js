464c2fd08bd3943454fa8235be5c8149f4e1eb61
Matias_Farias <mfmatias97@gmail.com>
2022-10-24 23:22:57 -0300

    falta acomodar contador

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const WORDS = []
-const WORDS2 = ["martillo","espada"]
 const wordContainer = document.getElementById('wordContainer');
 const startButton = document.getElementById('startButton');
 const usedLettersElement = document.getElementById('usedLetters');
@@ -7,44 +6,46 @@ const usedLettersElement = document.getElementById('usedLetters');
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
-ctx.canvas.width  = 0;
+ctx.canvas.width = 0;
 ctx.canvas.height = 0;
 
 const bodyParts = [
-    [4,2,1,1],
-    [4,3,1,2],
-    [3,5,1,1],
-    [5,5,1,1],
-    [3,3,1,1],
-    [5,3,1,1]
+    [4, 2, 1, 1],
+    [4, 3, 1, 2],
+    [3, 5, 1, 1],
+    [5, 5, 1, 1],
+    [3, 3, 1, 1],
+    [5, 3, 1, 1]
 ];
 
 let selectedWord;
 let usedLetters;
 let mistakes;
+let erroresJ1 = 0
+let erroresJ2 = 0
 let hits;
+let round = 0;
 
 
-fetch ('https://6345db9839ca915a6909468c.mockapi.io/ahorcado/words')
-.then((response) => response.json())
-.then((data) =>{
-  WORDS.push(data.map(probando => probando.word))
-  console.log(WORDS,"array")
-})
-.catch((error) => console.log(error))
+fetch('https://6345db9839ca915a6909468c.mockapi.io/ahorcado/words')
+    .then((response) => response.json())
+    .then((data) => {
+        WORDS.push(data.map(probando => probando.word))
+    })
+    .catch((error) => console.log(error))
 
-function swalGanador (){
+function swalGanador() {
     Swal.fire({
-        title:'Ganaste :)',
-        text:'Espero que te haya gustado',
+        title: 'Ganaste :)',
+        text: 'Espero que te haya gustado',
         icon: 'success'
     })
 }
 
-function swalPerdedor (){
+function swalPerdedor() {
     Swal.fire({
-        title:'Perdiste :(',
-        text:'Vuelve a intentarlo',
+        title: 'Perdiste :(',
+        text: 'Vuelve a intentarlo',
         icon: 'error'
     })
 }
@@ -61,13 +62,38 @@ const addBodyPart = (bodyPart) => {
     ctx.fillRect(...bodyPart)
 }
 
+
+
+
 const wrongLetter = () => {
+
     addBodyPart(bodyParts[mistakes])
     mistakes++
-    if(mistakes === bodyParts.length){
+    
+    erroresPlayers()
+    // if(mistakes === 6  && (round % 2) == 1){
+    //     erroresJ1++
+    // }else if(mistakes === 6 && (round % 2) == 0){
+    //     erroresJ2++
+    // }
+
+    if (mistakes === bodyParts.length) {
         endGame()
         swalPerdedor()
+        
     }
+
+}
+
+function erroresPlayers(){
+
+
+    if(mistakes == 6  && (round % 2) == 1){
+        erroresJ1++
+    }else if(mistakes == 6 && (round % 2) == 0){
+        erroresJ2++
+    }
+
 }
 
 
@@ -75,33 +101,35 @@ const endGame = () => {
     document.removeEventListener('keydown', letterEvent)
     startButton.style.display = 'block'
     localStorage.removeItem("word")
-    
+
 }
 
 const correctLetter = (letter) => {
-    const { children } = wordContainer
-    for(let i = 0; i < children.length; i++){
-        if(children[i].innerHTML === letter){
+    const {
+        children
+    } = wordContainer
+    for (let i = 0; i < children.length; i++) {
+        if (children[i].innerHTML === letter) {
             children[i].classList.toggle('hidden')
             hits++
         }
     }
-    if(hits === selectedWord.length){
+    if (hits === selectedWord.length) {
         endGame()
         swalGanador()
-    } 
+    }
 }
 
 const letterInput = (letter) => {
-    (selectedWord.includes(letter)) ? correctLetter(letter) : wrongLetter()
+    (selectedWord.includes(letter)) ? correctLetter(letter): wrongLetter()
 
     addLetter(letter)
     usedLetters.push(letter)
 }
 
-const  letterEvent = (event) => {
+const letterEvent = (event) => {
     let newLetter = (event).key.toUpperCase()
-    if(newLetter.match(/^[a-zñ]$/i) && !usedLetters.includes(newLetter)){
+    if (newLetter.match(/^[a-zñ]$/i) && !usedLetters.includes(newLetter)) {
         letterInput(newLetter)
     }
 }
@@ -118,8 +146,6 @@ const drawWord = () => {
 
         // letterJson = localStorage.getItem("letter")
 
-
-        
     });
 }
 
@@ -130,15 +156,13 @@ const selectRandomWord = () => {
     wordJSON = localStorage.getItem("word")
     let getWord = JSON.parse(wordJSON)
 
-    if(getWord == null){
-        console.log(WORDS,"hola")
-        console.log(WORDS2,"nuevo");
+    if (getWord == null) {
         word = WORDS[0][Math.floor(Math.random() * WORDS[0].length)].toUpperCase()
         selectedWord = word.split('')
         wordJSON = JSON.stringify(word)
         localStorage.setItem("word", wordJSON)
 
-    }else{
+    } else {
         selectedWord = getWord.split('')
     }
 }
@@ -146,8 +170,8 @@ const selectRandomWord = () => {
 const drawHangMan = () => {
     ctx.canvas.width = 120
     ctx.canvas.height = 160
-    ctx.scale(20,20)
-    ctx.clearRect(0,0, canvas.width, canvas.heigth)
+    ctx.scale(20, 20)
+    ctx.clearRect(0, 0, canvas.width, canvas.heigth)
     ctx.fillStyle = '#d95d39'
     ctx.fillRect(0, 7, 4, 1)
     ctx.fillRect(1, 0, 1, 8)
@@ -156,23 +180,23 @@ const drawHangMan = () => {
 }
 
 const startGame = () => {
-    usedLetters = []
-    mistakes = 0
-    hits = 0
-    wordContainer.innerHTML = ""
-    usedLettersElement.innerHTML = ""
-    startButton.style.display = "none"
-    drawHangMan()
-    selectRandomWord()
-    drawWord()
-    document.addEventListener('keydown', letterEvent)
-    letterEvent()
-    correctLetter()
-    letterInput()
-    endGame()
-    console.log(WORDS)
-}
 
+        usedLetters = []
+        mistakes = 0
+        hits = 0
+        wordContainer.innerHTML = ""
+        usedLettersElement.innerHTML = ""
+        startButton.style.display = "none"
+        drawHangMan()
+        selectRandomWord()
+        drawWord()
+        document.addEventListener('keydown', letterEvent)
+        letterEvent()
+        correctLetter()
+        letterInput()
+        endGame()
 
-startButton.addEventListener('click', startGame)
+    } 
+    
+    startButton.addEventListener('click', startGame)
 
--- multiplayer.js
+++ multiplayer.js
@@ -1,13 +1,74 @@
+let nombreJ1
+let nombreJ2
+
+
+function swalErrorPlayer() {
+    Swal.fire({
+        title: 'Faltan nombres',
+        icon: 'warning'
+    })
+}
 
 function getData() {
-    
-    const nombreJ1 = document.getElementById('nombreJ1').value 
-    const nombreJ2 = document.getElementById('nombreJ2').value
 
-    console.log(nombreJ1);
+    nombreJ1 = document.getElementById('nombreJ1').value
+    nombreJ2 = document.getElementById('nombreJ2').value
+    const modal = document.getElementById('exampleModal')
+    const form = document.getElementById('form')
+
+
+    localStorage.setItem("j1", nombreJ1)
+    localStorage.setItem("j2", nombreJ2)
+
+
+    if (nombreJ1 && nombreJ2) {
+        modal.className += "hidden"
+        form.reset()
+    } else {
+
+        Swal.fire({
+            title: 'Faltan nombres',
+            icon: 'warning',
+            backdrop: 'true'
+        })
+        return false;
+    }
+
+
 }
 
 
+
+
+let j1JSON = localStorage.getItem("j1")
+let j2JSON = localStorage.getItem("j2")
+let containerPlayers = document.getElementsByClassName('containerPlayers')
+let parrafoJ1 = document.getElementById('jugador1')
+let parrafoJ2 = document.getElementById('jugador2')
+
+if (j1JSON && j2JSON) {
+    nombreJ1 = j1JSON
+    nombreJ2 = j2JSON
+}
+
+let mistakesJ1 = document.getElementById('mistakesJ1')
+let mistakesJ2 = document.getElementById('mistakesJ2')
+
+function showPlayers() {
+    parrafoJ1.innerHTML = `<p>${nombreJ1}: ${erroresJ1}</p>`
+    parrafoJ1.className = "players"
+
+    parrafoJ2.innerHTML = `<p>${nombreJ2}: ${erroresJ2}</p>`
+    parrafoJ2.className = "players"
+
+}
+
+showPlayers()    
+
+function contadorClick(){
+    document.getElementById("contador").innerHTML = ++round
+    
+}
 
 
 
